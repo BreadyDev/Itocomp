8751fe4ad12da696aec3bc3b40017d4f69e441d2
BreadyDev <juantonioramos26@gmail.com>
2023-12-02 14:14:16 -0600

    Chat messages

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,53 @@
+"use client";
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const roleUser = "user";
+  const roleSys = "system";
+
+  const [input, setInput] = useState('');
+
+  const [messages, setMessages] = useState([
+    {
+      role: roleSys,
+      content: "¡Hola, soy ItoComp! Estoy aquí para ayudarte"
+    }
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMessage = {
+      role: roleUser,
+      content: input
+    };
+    const newMessages = [...messages, newMessage];
+    setInput("");
+    setMessages(newMessages);
+    sendMessageToApi(newMessages)
+  };  
+
+  const sendMessageToApi = async (messages) =>{
+    /*const response = await fetch('/api/chat', {
+      method: 'POST',
+      body: JSON.stringify({messages})
+    });
+
+    const data = await response.json();
+    setMessages(prevData => [...prevData, data.choices[0].message]);*/
+    setMessages(prevData => [...prevData, {
+      role: roleSys,
+      content: "Mensaje default"
+    }]);
+  }
+
+  const changeInput = (e) => {
+    setInput(e.currentTarget.value);
+  };
+
   return (
     <main className="flex min-h-screen flex-col">
 
@@ -22,24 +68,27 @@ export default function Home() {
 
       <div className="py-3 px-6 flex-grow">
 
-        <div className="chat chat-start">
-          <div className="chat-bubble chat-bubble-primary">¡Hola, soy ItoComp! Estoy aquí para ayudarte</div>
-        </div>
+      {
+        messages
+          .filter((message) => message.role === roleUser || message.role === roleSys)
+          .map((message, index) => (
+            <div className={`chat ${message.role === 'user' ? 'chat-end' : 'chat-start'}`} key={index}>
+              <div className="chat-bubble chat-bubble-primary">{message.content}</div>
+            </div>
+          ))
+      }
 
-        <div className="chat chat-end">
-          <div className="chat-bubble chat-bubble-primary ">Picate el ano, paro</div>
-        </div>
-        
+
       </div>
 
-      <div id="TextA" className="flex justify-between py-3 px-3">
-        <input id="textArea-prompt" type="text" placeholder="Type here" class="bg-transparent w-[95%] focus:outline-none"/>
-        <button class="btn btn-active">
+      <form id="TextA" className="flex justify-between py-3 px-3" onSubmit={handleSubmit}>
+        <textarea value={input} onChange={changeInput} placeholder="Escribe aqui..." id="textArea-prompt" class="bg-transparent w-[95%] focus:outline-none"></textarea>
+        <button class="btn btn-active" type="submit">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16" fill="none">
             <path d="M0.112811 2.4701C-0.145856 1.7621 0.0488106 0.978096 0.609477 0.473429C1.16881 -0.0292376 1.96548 -0.142571 2.64148 0.186096L14.7015 5.81543C15.3481 6.1181 15.8108 6.67076 15.9981 7.33076H2.69681L0.150811 2.55476C0.136811 2.52743 0.124144 2.49876 0.112811 2.4701ZM2.70614 8.66476L0.204144 13.4481C0.190144 13.4748 0.178144 13.5014 0.168144 13.5294C-0.0898561 14.2381 0.106811 15.0208 0.667477 15.5248C1.01481 15.8361 1.45348 15.9968 1.89481 15.9968C2.16748 15.9968 2.44081 15.9354 2.69548 15.8108L14.7028 10.1854C15.3515 9.88143 15.8135 9.32676 15.9995 8.66543H2.70614V8.66476Z" fill="white"/>
           </svg>
         </button>
-      </div>
+      </form>
 
     </main>
   )
